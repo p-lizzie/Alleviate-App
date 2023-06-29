845e75d3b8ce6d812282513e180298caf6753f21
Karen Canseco <kar3n.c35@gmail.com>
2023-06-29 12:37:05 -0700

    Start of Daily Journal Page

--- finalproject3/dailyjournal.swift
+++ finalproject3/dailyjournal.swift
@@ -9,7 +9,53 @@ import SwiftUI
 
 struct dailyjournal: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            (Color(red: 0.554, green: 0.631, blue: 0.562))
+                .edgesIgnoringSafeArea(.all)
+            
+            
+            RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Corner Radius@*/30.0/*@END_MENU_TOKEN@*/)
+                .fill(Color(red: 0.924, green: 0.777, blue: 0.711))
+                .frame(width: 300, height: 80)
+                .position(x: 195, y:100)
+            
+            Text ("Daily Journal")
+                .font(.title)
+                .position(x: 195, y:100)
+            
+            
+            RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Corner Radius@*/30.0/*@END_MENU_TOKEN@*/)
+                .fill(Color(red: 0.988, green: 0.945, blue: 0.851))
+                .frame(width: 300, height: 60)
+                .position(x: 195, y:250)
+            
+            Text ("How are you feeling today?")
+                .font(.title2)
+                .position(x: 195, y:250)
+            
+            HStack {
+                
+                Button("Button") {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                }
+                Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                }
+                Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                }
+                
+            }
+            
+            RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Corner Radius@*/30.0/*@END_MENU_TOKEN@*/)
+                .fill(Color(red: 0.988, green: 0.945, blue: 0.851))
+                .frame(width: 300, height: 60)
+                .position(x: 195, y:550)
+            
+            Text ("Why do you feel this way?")
+                .font(.title2)
+                .position(x: 195, y:550)
+        }
     }
 }
 
