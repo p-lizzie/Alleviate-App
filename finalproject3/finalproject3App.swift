b673755dfc9f6eb07d9aec3f7d4a45b35455470d
Paige <paige.e.leung@gmail.com>
2023-06-29 11:37:08 -0700

    Initial Commit

---
+++ finalproject3/finalproject3App.swift
@@ -0,0 +1,17 @@
+//
+//  finalproject3App.swift
+//  finalproject3
+//
+//  Created by Paige Leung on 6/29/23.
+//
+
+import SwiftUI
+
+@main
+struct finalproject3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
